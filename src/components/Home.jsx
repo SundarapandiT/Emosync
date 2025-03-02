59ed182152c1ee84156934561b297544f9fd7213
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-03-02 23:14:30 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
         <h1>Music <span className="highlight">Recommendation</span> System</h1>
         <p>Our AI-powered system reads your emotions and recommends the perfect playlist to match your vibe.
            Discover the music that understands you. Sync your emotions with <b>Emosync!</b></p>
-        <a href="/getemotion" className="glow-button">Get Started and Listen!</a>
+        <Link to="/getemotion" className="glow-button">Get Started and Listen!</Link>
       </div>
       {/* <div className='hero-image'>
         <img src="/emosync bg1.jpg" alt="Emosync UI" />
