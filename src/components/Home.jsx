7429319c26ab07a0056316f5d7ba1462b8774649
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-03-03 09:28:03 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <main className="hero-section">
       <div className='hero-text'>
         <h1>Music <span className="highlight">Recommendation</span> System</h1>
         <p>Our AI-powered system reads your emotions and recommends the perfect playlist to match your vibe.
            Discover the music that understands you. Sync your emotions with <b>Emosync!</b></p>
-        <Link to="/getemotion" className="glow-button">Get Started and Listen!</Link>
+         <button className="glow-button" onClick={() => navigate("/getemotion")}>
+          Get Started and Listen!
+        </button>
       </div>
       {/* <div className='hero-image'>
         <img src="/emosync bg1.jpg" alt="Emosync UI" />
